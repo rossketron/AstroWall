dd1412b5762ccca5c2a5983851e177c357e5b4bc
Ross Ketron <rossketron@gmail.com>
2021-01-27 20:29:40 -0500

    updated and completed script. Need to add to startup programs to run each time computer is started

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -2,26 +2,29 @@
 
 import requests
 import os
+import pwd
 
 # Read API key from keys file
-keyFile = open('keys', 'r')
+keyFile = open('keys.txt', 'r')
 key = keyFile.readline().rstrip()
 
 # Add your key from keys file to the API url
-url = 'https://api.nasa.gov/planetary/apod?api_key='+key
+url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 
 # Set wallpaper directory to store image
 global directory
+username = pwd.getpwuid(os.getuid()).pw_name
 directory = '/Users/'+username+'/Pictures/Wallpapers/'
 
 # Request APOD and write image to Wallpapers directory
 req = requests.get(url)
+print(url)
 if req:
-    APOD = r.json()['url']
+    APOD = req.json()['url']
     pic = requests.get(APOD, allow_redirects=True)
     if 'jpg' not in APOD:
         print('No image')
     else:
-        open(directory, 'wb').write(pic.content)
+        open(directory+'pod.jpg', 'wb').write(pic.content)
 else:
     print('Error getting Image')
